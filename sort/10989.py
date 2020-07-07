2a348ae54aa3509b520bd63abea33392dd19d72a
raeyoungii <skepo@naver.com>
2020-07-08 01:51:25 +0900

    10989.py

---
+++ sort/10989.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+cnt = [0] * 10001
+for i in range(N):
+    cnt[int(sys.stdin.readline())] += 1
+for i in range(10001):
+    if cnt[i] != 0:
+        for _ in range(cnt[i]):
+            print(i)
+
+# TODO: 이해필요
+# counting sort
