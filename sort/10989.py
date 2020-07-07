fe6278d3d6d8d1f5d45da287958016048a2e81c5
raeyoungii <skepo@naver.com>
2020-07-08 01:51:51 +0900

    10989.py

--- sort/10989.py
+++ sort/10989.py
@@ -2,7 +2,7 @@ import sys
 
 N = int(sys.stdin.readline())
 cnt = [0] * 10001
-for i in range(N):
+for _ in range(N):
     cnt[int(sys.stdin.readline())] += 1
 for i in range(10001):
     if cnt[i] != 0:
