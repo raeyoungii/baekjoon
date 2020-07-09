85a2bc6e61814b71bcddd4f4a3bbc6fdfe977c8b
raeyoungii <skepo@naver.com>
2020-07-09 16:06:31 +0900

    10989.py

--- dp/2011.py
+++ dp/2011.py
@@ -6,6 +6,6 @@ for i in range(1, len(N)):
     if i == 1:
         continue
     x = N[i - 1] * 10 + N[i]
-    if (10 <= x) and (x <= 26):
+    if 10 <= x <= 26:
         d[i] += d[i - 2]
 print(d[len(N) - 1] % 1000000)
